3a3bcfb106fcb947a584724d33ebd0147f65390f
Maria De Angelis <mariadeangelis@fb.com>
2017-06-22 13:52:35 -0700

    Added MovieDetailActivity rating functionality

---
+++ app/src/main/java/com/codepath/flickster/MovieDetailsActivity.java
@@ -0,0 +1,52 @@
+package com.codepath.flickster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.flickster.R;
+import com.codepath.flickster.models.Movie;
+
+import org.parceler.Parcel;
+import org.parceler.Parcels;
+
+import static com.codepath.flickster.R.id.tvOverview;
+import static com.codepath.flickster.R.id.tvTitle;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    // the movie we are zooming in on
+    Movie movie;
+
+    // the view objects
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        // resolve the view objects
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+
+        // unwrap the movie passes in via intent, using its simple name as a key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // set the title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        // vote average is 0..10, convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
+
+    }
+}
