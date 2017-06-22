a01fc4f1d893a59a23de9e1b68a675392f70b217
Maria De Angelis <mariadeangelis@fb.com>
2017-06-22 15:42:15 -0700

    Added optimizations for landscape mode, all files

--- app/src/main/java/com/codepath/flickster/models/Config.java
+++ app/src/main/java/com/codepath/flickster/models/Config.java
@@ -16,6 +16,9 @@ public class Config
     // the poster size to use when fetching images, part of the url
     String posterSize;
 
+    // the backdrop size to use when fetching images
+    String backdropSize;
+
     public String getImageBaseUrl() {
         return imageBaseUrl;
     }
@@ -30,6 +33,10 @@ public class Config
         return String.format("%s%s%s", imageBaseUrl, size, path); // join all three
     }
 
+    public String getBackdropSize() {
+        return backdropSize;
+    }
+
     public Config(JSONObject object) throws JSONException
     {
         JSONObject images = object.getJSONObject("images");
@@ -41,5 +48,9 @@ public class Config
 
         // use the option at index 3 : w342
         posterSize = posterSizeOptions.optString(3, "w342");
+
+        // parse backdrop sizes and uses the option at index 1
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
     }
 }
--- app/src/main/java/com/codepath/flickster/models/Movie.java
+++ app/src/main/java/com/codepath/flickster/models/Movie.java
@@ -2,24 +2,43 @@ package com.codepath.flickster.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 /**
  * Created by mariadeangelis on 6/21/17.
  */
 
 // track all info for movie to display
+@Parcel   // annotation class is Parcelable!
 public class Movie {
 
-    // values from API
+    // values from API, public for parceler
     private String title;
     private String overview;
     private String posterPath; // only the path
+    private String backdropPath;
+
+    Double voteAverage; // value returned from API
+
+    //  no arg contructor required for Par.
+    public Movie(){}
 
     // initialize the JSON data
     public Movie(JSONObject object) throws JSONException {
         title = object.getString("title");
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
+        backdropPath = object.getString("backdrop_path");
+
+        voteAverage = object.getDouble("vote_average");
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 
     public String getTitle() {
